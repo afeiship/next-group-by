216934228e125d16bcd1f64875db769101e67038
afeiship <1290657123@qq.com>
2019-02-26 17:39:07 +0800

    fix: add prettier

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # next-group-by
 > Creates an object composed of keys generated from the results of running each element of collection thru iteratee. 
 
-
 ## usage:
 ```js
 var arr1 = [1,2,3,4,5,6,7,8,9];
--- src/next-group-by.js
+++ src/next-group-by.js
@@ -1,21 +1,18 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
 
-  nx.groupBy = function (inArray, inCallback) {
+  nx.groupBy = function(inArray, inCallback) {
     var result = {};
     for (var index = 0; index < inArray.length; index++) {
       var element = inArray[index];
       var value = inCallback.call(inArray, index, element, inArray);
-      result[value] = (result[value] || []).concat( element );
+      result[value] = (result[value] || []).concat(element);
     }
     return result;
   };
 
-
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.groupBy;
   }
-
-}());
+})();
