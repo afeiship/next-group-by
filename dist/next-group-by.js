85afe790ab1b79bf96feec4f90220a6977037191
afeiship <1290657123@qq.com>
2020-03-31 23:49:03 +0800

    fix: remove test.json

--- dist/next-group-by.js
+++ dist/next-group-by.js
@@ -3,15 +3,15 @@
  * description: Creates an object composed of keys generated from the results of running each element of collection thru iteratee.
  * url: https://github.com/afeiship/next-group-by
  * version: 1.0.0
- * date: 2020-03-31 23:03:55
+ * date: 2020-03-31 23:48:46
  * license: MIT
  */
 
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.groupBy = function(inArray, inCallback) {
+  nx.groupBy = function (inArray, inCallback) {
     var result = {};
     for (var index = 0; index < inArray.length; index++) {
       var element = inArray[index];
